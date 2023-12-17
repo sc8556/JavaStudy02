91b9c4b510f6089f81955228d69f0f9e6037492e
Jay <sc8556@naver.com>
2023-12-18 00:41:23 +0900

    2023.12.18 GitHub 연동 및 브랜치 변경 테스트 02

--- src/GitHubTest.java
+++ src/GitHubTest.java
@@ -1,5 +1,6 @@
 public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("Test For GitHub Connection And Branch Changes");
+        System.out.println("Test For GitHub Connection And Branch Changes");
     }
 }
